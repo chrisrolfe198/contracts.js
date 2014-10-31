1783e57a01c5b7968bece756ac2cb5b52c635763
Christopher Rolfe <christopher.rolfe198@gmail.com>
2014-10-31 18:59:32 +0000

    Adding tests for the add method method on the contract

--- src/contract.js
+++ src/contract.js
@@ -2,11 +2,15 @@ var repository = require('./contractRepository');
 
 function contract(name) {
 	this.name = name;
+	this.methods = {};
 }
 
 contract.prototype.addMethod = function(name, args) {
-	this.name = name;
-	this.arguments = args;
+	if (typeof args === 'string') {
+		args = [args];
+	}
+
+	this.methods[name] = args;
 };
 
 contract.prototype.bind = function(name) {
--- test/contract_test.js
+++ test/contract_test.js
@@ -9,6 +9,22 @@ describe('contract', function () {
 
     it('should correctly add a name property to the contract', function () {
       TestContract.should.have.property('name', 'TestContract');
+    });
+
+  });
+
+  describe('addMethod()', function() {
+
+    it('should add a method and an array of arguments for a contract to meet', function() {
+      TestContract.addMethod("testMethod", ["foobar", "baz"]);
+
+      TestContract.methods.should.eql({ "testMethod" : ["foobar", "baz"] });
+    });
+
+    it('should add a method and a string argument for the contract to meeet', function() {
+      TestContract.addMethod("testMethod", "foobar");
+
+      TestContract.methods.should.eql({ "testMethod" : ["foobar"] });
     })
 
   });
